ba3c3d0549f7463ce724f4d19e23730580be8fdb
yappippipiyya <yappippipiyya@gmail.com>
2025-10-05 14:54:32 +0900

    constバグ直した

--- const.py
+++ const.py
@@ -11,6 +11,9 @@ DOTENV_DIR = os.path.join(BASE_DIR, ".env")
 
 load_dotenv(DOTENV_DIR, override=True)
 
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 HOST = os.getenv("host", "")
 USER = os.getenv("user", "")
 PASSWORD = os.getenv("password", "")
@@ -23,4 +26,3 @@ LINE_USER_INFO_URL = 'https://api.line.me/v2/profile'
 
 CLIENT_ID = os.environ.get("cliend_id")
 CLIENT_SECRET = os.environ.get("client_secret")
-SECRET_KEY = os.environ.get("SECRET_KEY")
